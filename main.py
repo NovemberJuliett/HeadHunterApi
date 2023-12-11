31eb09a008b4d4e7c795395ffe39646f77948feb
NovemberJuliett <137638315+NovemberJuliett@users.noreply.github.com>
2023-12-11 20:56:22 +0200

    Create function predict_rub_salary

--- main.py
+++ main.py
@@ -11,28 +11,30 @@ for language in languages_list:
     response = requests.get(base_url, params=params)
     count = response.json()
     languages_dict[language] = count["found"]
-print(languages_dict)
 
 
-params = {"area": "1",
-          "text": "Python"}
-response = requests.get(base_url, params=params)
-vacancy_features = response.json()["items"]
-for feature in vacancy_features:
-    salary = feature["salary"]
-    print(salary)
+def predict_rub_salary(vacancy):
+    response = requests.get(vacancy)
+    vacancy_features = response.json()["items"]
+    for feature in vacancy_features:
+        salary = feature["salary"]
+        if not salary:
+            print(None)
+            continue
+        salary_from = salary["from"]
+        salary_to = salary["to"]
+        if salary_from and salary_to:
+            average = (salary_from + salary_to) / 2
+            print(average)
+        else:
+            if not salary_from:
+                to_rate = salary_to * 0.8
+                print(to_rate)
+            if not salary_to:
+                from_rate = salary_from * 1.2
+                print(from_rate)
+            if salary["currency"] != "RUR":
+                print(None)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+predict_rub_salary("https://api.hh.ru/vacancies?area=1&text=Python")

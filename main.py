87d68d07eebea673372d433e462f7061527eb616
NovemberJuliett <137638315+NovemberJuliett@users.noreply.github.com>
2023-12-11 18:49:37 +0200

    Count salary for Python devs

--- main.py
+++ main.py
@@ -2,16 +2,26 @@ import requests
 from pprint import pprint
 
 languages_list = ["JavaScript", "Java", "Python", "PHP", "C++", "C#", "C", "Go", "Shell"]
-base_url = 'https://api.hh.ru/vacancies'
-
+base_url = "https://api.hh.ru/vacancies"
 
 languages_dict = {}
 for language in languages_list:
-    link = f"{base_url}?area=1&text={language}"
-    response = requests.get(link)
-    count = response.json()["found"]
-    languages_dict[language] = count
+    params = {"area": "1",
+              "text": f"{language}"}
+    response = requests.get(base_url, params=params)
+    count = response.json()
+    languages_dict[language] = count["found"]
 print(languages_dict)
+
+
+params = {"area": "1",
+          "text": "Python"}
+response = requests.get(base_url, params=params)
+vacancy_features = response.json()["items"]
+for feature in vacancy_features:
+    salary = feature["salary"]
+    print(salary)
+
 
 
 

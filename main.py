7f1a5e46956c540860aec61056ef29637a6de402
NovemberJuliett <137638315+NovemberJuliett@users.noreply.github.com>
2023-12-27 17:12:59 +0200

    edit first requests to superjob API

--- main.py
+++ main.py
@@ -9,19 +9,8 @@ url = "https://api.superjob.ru/2.0/vacancies"
 
 headers = {"X-Api-App-Id": token}
 params = {
-    "town": {
-        'declension': 'в Москве',
-                       'genitive': 'Москвы',
-                       'hasMetro': True,
-                       'id': 4,
-                       'title': 'Москва',
-    },
-    "catalogues": {"title_rus":"Разработка, программирование",
-                   "url_rus":"razrabotka-po",
-                   "title":"Разработка, программирование",
-                   "id_parent":33,
-                   "key":48}
-}
+    "town": 4,
+    "catalogues": 33}
 response = requests.get(url, headers=headers, params=params)
 data = response.json()
 pprint(data)
